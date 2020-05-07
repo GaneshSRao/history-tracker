4c76f2af5069ed7c020de20a7d147c7cccd8210d
GaneshSRao <Ganesh.Shreedhar@spartasystems.com>
2020-05-07 12:57:48 +0530

    Added Trigger Factory

--- force-app/main/default/lwc/changeHistorySetup/changeHistorySetup.js
+++ force-app/main/default/lwc/changeHistorySetup/changeHistorySetup.js
@@ -1,46 +1,23 @@
 import { LightningElement, track, wire } from "lwc";
 import getObjectsList from "@salesforce/apex/ChangeHistorySetupController.getObjectsList";
 
-// const objects2 = [
-//   { label: "Account", name: "Account" },
-//   { label: "Contact", name: "Contact" },
-//   { label: "Opportunity", name: "Opportunity" },
-//   { label: "Quote", name: "Quote" },
-//   { label: "Product", name: "Product" },
-//   { label: "Campaign", name: "Campaign" },
-//   { label: "Lead", name: "Lead" },
-//   { label: "Case", name: "Case" },
-//   { label: "Knowledge", name: "Knowledge" },
-//   { label: "Asset", name: "Asset" },
-//   { label: "Contract", name: "Contract" },
-//   { label: "Order", name: "Order" },
-//   { label: "Student", name: "Student__c" },
-//   { label: "Candidate", name: "Candidate__c" },
-//   { label: "Account", name: "Account2" },
-//   { label: "Contact", name: "Contact2" },
-//   { label: "Opportynity", name: "Opportynity2" },
-//   { label: "Quote", name: "Quote2" },
-//   { label: "Product", name: "Product2" },
-//   { label: "Campaign", name: "Campaign2" },
-//   { label: "Lead", name: "Lead2" },
-//   { label: "Case", name: "Case2" },
-//   { label: "Knowledge", name: "Knowledge2" },
-//   { label: "Asset", name: "Asset2" },
-//   { label: "Contract", name: "Contract2" },
-//   { label: "Order", name: "Order2" },
-//   { label: "Student", name: "Student2__c" },
-//   { label: "Candidate", name: "Candidate2__c" }
-// ];
-
 export default class ChangeHistorySetup extends LightningElement {
   // @track objects = objects;
   @wire(getObjectsList)
   objects;
 
   @track selectedItem = 'Account';
-  @track currentContent = '';
+  @track currentobjectLabel = 'Account';
+  @track currentObjectApi = 'Account';
 
   handleSelect(event) {
-    this.currentContent = event.detail.name;
+    this.currentObjectApi = event.detail.name;
+    // extract selected object name
+    if(this.objects.data){
+      const currentobjects = this.objects.data.filter(obj => {
+        return obj.name === this.currentObjectApi;
+      });
+      this.currentobjectLabel = currentobjects[0].label;
+    }
   }
 }
--- force-app/main/default/lwc/setupFieldList/setupFieldList.js
+++ force-app/main/default/lwc/setupFieldList/setupFieldList.js
@@ -1,34 +1,96 @@
-import { LightningElement, api, wire } from 'lwc';
-import getFieldsByObjectName from '@salesforce/apex/ChangeHistorySetupController.getFieldsByObjectName';
+import { LightningElement, track, api, wire } from "lwc";
+import getFieldsByObjectName from "@salesforce/apex/ChangeHistorySetupController.getFieldsByObjectName";
+import createConfigRecord from "@salesforce/apex/ChangeHistorySetupController.createConfigRecord";
+import { refreshApex } from "@salesforce/apex";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-// const options = [
-//     { label: 'Account Name', value: 'AccountId' , selected:true},
-//     { label: 'Amount', value: 'Amount' ,selected:true},
-//     { label: 'Close Date', value: 'CloseDate' ,selected:true},
-//     { label: 'Contract', value: 'ContractId' ,selected:false},
-//     { label: 'Created By', value: 'CreatedById' ,selected:false},
-//     { label: 'Forecast Category Name', value: 'ForecastCategoryName' ,selected:true},
-//     { label: 'Last Modified By', value: 'LastModifiedById' ,selected:false},
-//     { label: 'Lead Source', value: 'LeadSource' ,selected:false},
-//     { label: 'Next Step', value: 'NextStep' ,selected:false},
-//     { label: 'Opportunity Name', value: 'OpportunityName' ,selected:false},
-//     { label: 'Opportunity Owner', value: 'OpportunityOwner' ,selected:false},
-//     { label: 'Primary Campaign Source', value: 'CampaignSource' ,selected:false},
-//     { label: 'Probability', value: 'Probability' ,selected:false},
-//     { label: 'Score', value: 'Score__c' ,selected:false},
-//     { label: 'Stage', value: 'Stage' ,selected:false},
-//     { label: 'Type', value: 'Type' ,selected:false},
-//     { label: 'Description', value: 'Description' ,selected:false},
-// ];
+export default class SetupFieldList extends LightningElement {
+  @api 
+  get objectapi(){
+    return this._objectapi;
+  }
+  set objectapi(value){
+      this.isloading = true;
+      this._objectapi = value;
+  }
 
-export default class App extends LightningElement {
+  @api objectname;
+  
+  @track disableRefresh = false;
+  @track optionsList;
+  @track isloading = true;
+  
+  _objectapi;
 
-    @api objectapi;
-    @wire(getFieldsByObjectName, {objectApi:"$objectapi"})
-    options;
-    // @track options = options; 
+  /** Wired Apex result so it can be refreshed programmatically */
+  originalList;
 
-    clickHandler(e){
-        alert(e.target.value + e.target.checked);
+  @wire(getFieldsByObjectName, { objectApi: "$objectapi" })
+  options(result) {
+    this.originalList = result;
+    if (result.data) {
+      this.optionsList = result.data;
+      this.isloading = false;
+    } else if (result.error) {
+      this.error = result.error;
     }
+  }
+
+  clickHandler(e) {
+    // alert(e.target.value + e.target.checked);
+    this.disableRefresh = false;
+    const temp = this.optionsList.map(obj => {
+      if (obj.value === e.target.value) {
+        obj = { ...obj, selected: e.target.checked };
+        // console.log(obj);
+      }
+      return obj;
+    });
+    // console.log(temp);
+    this.optionsList = temp;
+  }
+
+  handleSave() {
+    // return refreshApex();
+    const temp = this.optionsList.filter(obj => {
+      return obj.selected === true;
+    });
+
+    const trackedFields = temp.map(a => a.value).toString();
+    //alert(trackedFields);
+    createConfigRecord({
+      objectApi: this.objectapi,
+      trackedFields: trackedFields
+    })
+      .then(() => {
+        //alert(result);
+        this.disableRefresh = true;
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: this.objectname+' Configuration Updated Successfully',
+                variant: 'success'
+            })
+        );
+        return refreshApex(this.originalList);
+      })
+      .catch(error => {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Error',
+                message: 'Configuration Updation Failed',
+                variant: 'error'
+            })
+        );
+        this.error = error;
+      });
+  }
+
+  handleRefresh() {
+    this.disableRefresh = true;
+    this.optionsList =
+      this.originalList && this.originalList.data
+        ? this.originalList.data
+        : this.optionsList;
+  }
 }
